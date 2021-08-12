42b5ad648eb271f0b16217c1d601f0c38506d83d
shubham7090 <shubham.agg70@gmail.com>
2021-08-12 18:13:20 +0530

    next Smaller Elements on Left using stacks

---
+++ stack/nextSmallerElementLeft.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class nextSmallerElementLeft {
+  public static void display(int[] a) {
+    StringBuilder sb = new StringBuilder();
+
+    for (int val : a) {
+      sb.append(val + "\n");
+    }
+    System.out.println(sb);
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] a = new int[n];
+    for (int i = 0; i < n; i++) {
+      a[i] = Integer.parseInt(br.readLine());
+    }
+
+    int[] nge = solve(a);
+    display(nge);
+  }
+
+  public static int[] solve(int[] arr) {
+    // solve
+    int[] ans=new int[arr.length];
+    LinkedList<Integer> st=new LinkedList<Integer>();
+    ans[0]=-1;
+    st.add(arr[0]);
+    for(int i=1;i<arr.length;i++){
+        while(st.size()>0&&arr[i]<st.getLast())st.removeLast();
+        if(st.size()==0)ans[i]=-1;
+        else ans[i]=st.getLast();
+        st.add(arr[i]);
+    }
+    return ans;
+  }
+
+}
